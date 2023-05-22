cbf1a1d515548c9015626404fc980184e2386b84
marwanalrayes2023 <marwanalrayes072@gmail.com>
2023-05-22 13:35:11 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:task_1/Screen_2.dart';
+
+import 'Screen_1.dart';
+
+void main() {
+  runApp(const MaterialApp(home:Screen_2()));
+
+}
+// ignore: camel_case_types
